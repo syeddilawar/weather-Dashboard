46c16b34b34ae33a70acb407d1aa92e783a76cd3
syed dilawar <wasimdilawer@gmail.com>
2020-08-17 14:55:38 -0700

    scripting

---
+++ script.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+  const apikey = "166a433c57516f51dfab1f7edaed8413";
+  let cities = [];
+
+  function cityinfo() {
+    let city = $(this).attr("data-name");
+    let queryurl =
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+      city +
+      "&units=imperial&appid=" +
+      apikey;
+    $.get(queryurl, function (response) {
+      $("#selectedCity").text(response.name);
+      $("#weatherIcon").attr(
+        "src",
+        "http://openweathermap.org/img/wn/" +
+          response.weather[0].icon +
+          "@2x.png"
+      );
+      $("#currentDate").text(moment().format("MMM DO, YYY"));
+      "#windspeed".text(response.wind.speed + "mph");
+    });
+  }
+});
