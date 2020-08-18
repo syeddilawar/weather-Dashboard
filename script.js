16cdb90d758db74ace465900685e8c5701afaa84
syed dilawar <wasimdilawer@gmail.com>
2020-08-17 17:01:16 -0700

    styling and fixing js file

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
   const apikey = "166a433c57516f51dfab1f7edaed8413";
   let cities = [];
 
-  function cityinfo() {
+  function CityInfo() {
     let city = $(this).attr("data-name");
     let queryurl =
       "https://api.openweathermap.org/data/2.5/weather?q=" +
@@ -10,23 +10,23 @@ $(document).ready(function () {
       "&units=imperial&appid=" +
       apikey;
     $.get(queryurl, function (response) {
-      $("#selectedCity").text(response.name);
-      $("#weatherIcon").attr(
+      $("#City").text(response.name);
+      $("#weather").attr(
         "src",
         "http://openweathermap.org/img/wn/" +
           response.weather[0].icon +
           "@2x.png"
       );
-      $("#currentDate").text(moment().format("MMM DO, YYY"));
+      $("#currentDate").text(moment().format("MMM DA, YYY"));
       $("#windspeed").text(response.wind.speed + "mph");
       $("#currentTemp").text(response.main.temp);
       $("#humidty").text(response.main.humidity + "%");
       let lon = response.coord.lon;
       let lat = response.coord.lat;
-      getFiveDayForecast(lon, lat);
+      getDaysForecast(lon, lat);
     });
   }
-  function getFiveDayForecast(lon, lat) {
+  function getDaysForecast(lon, lat) {
     $.get(function (response) {
       $("#uvIndex").text(response.current.uvi);
       if (response.current.uvi <= 3) {
@@ -38,7 +38,77 @@ $(document).ready(function () {
       } else {
         $("#uvIndex").css("background-color", "orange");
       }
-      renderFiveDay(response.daily);
+      FiveDay(response.daily);
     });
   }
+  function FiveDay(arr) {
+    $(".DaysContainers").empty();
+
+    for (let i = 1; i < 4; i++) {
+      const forecastDays = $("<div>").addClass("forecastDays");
+      const forecastDate = $("<p>").text(
+        moment().add(i, "days").format("ddd-Do")
+      );
+      const forecastIcon = $("<img>").attr(
+        "src",
+        "http://openweathermap.org/img/wn/" + arr[i].weather[0].icon + "@2x.png"
+      );
+      const forecastTemp = $("<p>").text("Temp: " + arr[i].temp.day);
+      const forecastHumidity = $("<p>").text("Humidity: " + arr[i].humidity);
+      forecastDays.append(
+        forecastDate,
+        forecastIcon,
+        forecastTemp,
+        forecastHumidity
+      );
+      $(".DaysContainers").append(forecastDays);
+    }
+  }
+
+  function renderCityButtons() {
+    $("#cityButtonsContainer").empty();
+    for (var i = 0; i < cities.length; i++) {
+      var cityButton = $("<button>");
+
+      cityButton.addClass("city");
+
+      cityButton.attr("data-name", cities[i]);
+
+      cityButton.text(cities[i]);
+
+      $("#cityButtonsContainer").append(cityButton);
+    }
+  }
+
+  $("#searchCity").on("click", function (event) {
+    event.preventDefault();
+
+    if ($("#cityInput").val() == "") {
+      return false;
+    }
+    var city = $("#cityInput").val().trim();
+
+    cities.push(city);
+    renderCityButtons();
+
+    $("#cityInput").val("");
+    storeCityInfo();
+  });
+
+  $(document).on("click", ".city", CityInfo);
+
+  function storeCityInfo() {
+    localStorage.setItem("cities", JSON.stringify(cities));
+  }
+
+  function getCities() {
+    if (localStorage.getItem("cities")) {
+      const savedCities = JSON.parse(localStorage.getItem("cities"));
+      cities.push(...savedCities);
+    }
+  }
+  getCities();
+  renderCityButtons();
+
+  $("button:last-child").click();
 });
